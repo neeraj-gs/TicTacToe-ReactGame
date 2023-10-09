5a232acd8a27e04a294c2885573b6e53d04f72f7
neeraj-gs <gsneeraj05@gmail.com>
2023-10-09 22:13:08 +0530

    INitilaised an empty react proejct

---
+++ tictactoe/src/TicTacToeGame/Board.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Board = () => {
+  return (
+    <div className='board-container'>
+      <div className='board-row'></div>
+      <div className='board-row'></div>
+      <div className='board-row'></div>
+    </div>
+  )
+}
+
+export default Board
