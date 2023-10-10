308c64e05d58dc4f9e648b32acabe9cb98ecfbff
neeraj-gs <gsneeraj05@gmail.com>
2023-10-10 08:07:30 +0530

    Added our initial UI onto our Page

--- tictactoe/src/TicTacToeGame/Square.jsx
+++ tictactoe/src/TicTacToeGame/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Square = () => {
   return (
     <div 
-    style={{border:"1px solid",height:"100px",width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}
+    style={{border:"1px solid",height:"200px",width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}
     className='square'>
       <h5>X</h5>
     </div>
