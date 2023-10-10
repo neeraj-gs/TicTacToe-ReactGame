56ed994d75cf32d3791dfe01846157146c99661b
neeraj-gs <gsneeraj05@gmail.com>
2023-10-10 08:31:31 +0530

    Added a CLick event listener each square

--- tictactoe/src/TicTacToeGame/Board.jsx
+++ tictactoe/src/TicTacToeGame/Board.jsx
@@ -5,23 +5,28 @@ import Square from './Square'
 const Board = () => {
 
   const [state,setState] = useState(Array(9).fill(null))
-  console.log(state)
+
+  const handleCLick=(index)=>{
+    console.log('Clicked on',index)
+  }
+
+
   return (
     <div className='board-container'>
       <div className='board-row'>
-        <Square value={state[0]} />
-        <Square value={state[1]}/>
-        <Square value={state[2]}/>
+        <Square onClick={()=>handleCLick(0)} value={state[0]} />
+        <Square onClick={()=>handleCLick(1)} value={state[1]}/>
+        <Square onClick={()=>handleCLick(2)} value={state[2]}/>
       </div>
       <div className='board-row'>
-      <Square value={state[3]}/>
-      <Square value={state[4]}/>
-      <Square value={state[5]}/>
+      <Square onClick={()=>handleCLick(3)} value={state[3]}/>
+      <Square onClick={()=>handleCLick(4)} value={state[4]}/>
+      <Square onClick={()=>handleCLick(5)} value={state[5]}/>
       </div>
       <div className='board-row'>
-      <Square value={state[6]}/>
-      <Square value={state[7]}/>
-      <Square value={state[8]}/>
+      <Square onClick={()=>handleCLick(6)} value={state[6]}/>
+      <Square onClick={()=>handleCLick(7)} value={state[7]}/>
+      <Square onClick={()=>handleCLick(8)} value={state[8]}/>
       </div>
     </div>
   )
