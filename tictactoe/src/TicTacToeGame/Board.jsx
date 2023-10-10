ee616ca23b4c51497ee2da3a74fcd5c907a71a9c
neeraj-gs <gsneeraj05@gmail.com>
2023-10-10 08:14:10 +0530

    Added a State for each square and assigned it to array index and made it available to everyone

--- tictactoe/src/TicTacToeGame/Board.jsx
+++ tictactoe/src/TicTacToeGame/Board.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 
 
 const Board = () => {
+
+  const [state,setState] = useState(Array(9).fill(null))
+  console.log(state)
   return (
     <div className='board-container'>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={state[0]} />
+        <Square value={state[1]}/>
+        <Square value={state[2]}/>
       </div>
       <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
+      <Square value={state[3]}/>
+      <Square value={state[4]}/>
+      <Square value={state[5]}/>
       </div>
       <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
+      <Square value={state[6]}/>
+      <Square value={state[7]}/>
+      <Square value={state[8]}/>
       </div>
     </div>
   )
--- tictactoe/src/TicTacToeGame/Square.jsx
+++ tictactoe/src/TicTacToeGame/Square.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Square = () => {
+const Square = (props) => {
   return (
     <div 
     style={{border:"1px solid",height:"200px",width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}
     className='square'>
-      <h5>X</h5>
+      <h5>{props.value}</h5>
     </div>
   )
 }
