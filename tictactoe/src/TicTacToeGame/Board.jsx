5037ed1a214e7dcb0a152dad90c9f4d8da575433
neeraj-gs <gsneeraj05@gmail.com>
2023-10-10 07:58:03 +0530

    Added a Board componenet and then added a Square to it

--- tictactoe/src/TicTacToeGame/Board.jsx
+++ tictactoe/src/TicTacToeGame/Board.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Square from './Square'
+
 
 const Board = () => {
   return (
     <div className='board-container'>
-      <div className='board-row'></div>
-      <div className='board-row'></div>
-      <div className='board-row'></div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+      <Square />
+      <Square />
+      <Square />
+      </div>
+      <div className='board-row'>
+      <Square />
+      <Square />
+      <Square />
+      </div>
     </div>
   )
 }
